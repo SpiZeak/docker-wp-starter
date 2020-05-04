31d7611a1194de9c77853e4feb25ef4d34318a94
SpiZeak <max.t96@hotmail.com>
2020-05-04 23:04:12 +0200

    Edit: Configuration

--- www/web/app/themes/sage/run.sh
+++ www/web/app/themes/sage/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Install Bedrock, Wordpress and plugin dependencies
-(cd ../../../../ && composer install)
+set -e
 
-# Install Sage dependencies
-composer install
+echo -ne '\n\033[0;37mInstalling Bedrock, Wordpress and plugin dependencies with Composer ... '
+(cd ../../../../ && composer install  --quiet --no-interaction)
+echo -ne '\033[0;32mdone'
 
-# Install Node packages with Yarn
-npm install yarn --no-package-lock && npx yarn
+echo -ne '\n\033[0;37mInstalling Sage dependencies with Composer ... '
+composer install --quiet --no-interaction
+echo -ne '\033[0;32mdone'
 
-# Stop all running containers
-docker stop $(docker ps -a -q)
+echo -ne '\n\033[0;37mInstalling Node packages with Yarn ... '
+(npm install yarn --quiet --no-audit --no-package-lock && npx yarn --silent) > /dev/null 2>&1
+echo -ne '\033[0;32mdone'
+
+echo -ne '\n\033[0;37mStopping all running Docker containers ... '
+docker stop $(docker ps -a -q) > /dev/null 2>&1
+echo -ne '\033[0;32mdone\033[0;37m\n'
 
 # Run Docker containers and start development server
 (cd ../../../../../ && docker-compose up -d)
 
 # Start development server
-npx yarn start
+npx --no-install --quiet yarn start
